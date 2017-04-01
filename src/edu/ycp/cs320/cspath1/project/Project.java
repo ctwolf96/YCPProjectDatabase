26ba577c0f173b14fc79eab502b39f58b387392e
Corey Wolf <cwolf11@ycp.edu>
2017-04-01 17:47:04 -0400

    dirty worktree again

--- src/edu/ycp/cs320/cspath1/project/Project.java
+++ src/edu/ycp/cs320/cspath1/project/Project.java
@@ -1,21 +1,20 @@
 package edu.ycp.cs320.cspath1.project;
 
 import edu.ycp.cs320.cspath1.user.User;
-import java.util.ArrayList;
 
 public class Project {
 	// How are we linking a project with a user? At first I was thinking of using an arraylist of user id's but that only applies
-	// to active projects who have members. A proposal/solicitation will only have one user (creator) to link a project and user together
-	// I guess what I'm getting at is should we link a project and user by creator and then worry about linking the members of an active
-	// project later
+		// to active projects who have members. A proposal/solicitation will only have one user (creator) to link a project and user together
+		// I guess what I'm getting at is should we link a project and user by creator and then worry about linking the members of an active
+		// project later
 	private User creator;
 	private String title;
 	private String description;
+	private int UserID;
 	private int ProjectID;
 	
 	//Constructor
 	public Project() {
-		
 	}
 	
 	//Setters
@@ -27,6 +26,9 @@ public class Project {
 	}
 	public void setDescription(String description) {
 		this.description = description;
+	}
+	public void setUserID(int userID) {
+		UserID = userID;
 	}
 	public void setProjectID(int projectID) {
 		ProjectID = projectID;
@@ -41,6 +43,9 @@ public class Project {
 	}
 	public String getDescription() {
 		return description;
+	}
+	public int getUserID() {
+		return UserID;
 	}
 	public int getProjectID() {
 		return ProjectID;
